9d6322b42c409083a1c7a47471f359e92c39e87e
grigorevpv <grigorevpv@gmail.com>
2018-08-05 12:59:09 +0300

    make reflection model

---
+++ LingAnyTranslator/Reflections/Reflection.ts
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { Language } from '../Languages/Language';
+
+export class Reflection {
+
+  public Id: uuidv4;
+
+  public Href: string;
+
+  public Title: string;
+
+  public ForeignLanguage: Language;
+
+  public NativeLanguage: Language;
+
+  public fromJSON(data: object) {
+    this.Id = data['id'];
+    this.Href = data['href'];
+    this.Title = data['title'];
+    this.ForeignLanguage = data['foreignLanguage'] && new Language().fromJSON(data['foreignLanguage']);
+    this.NativeLanguage = data['nativeLanguage'] && new Language().fromJSON(data['nativeLanguage']);
+  }
+}
